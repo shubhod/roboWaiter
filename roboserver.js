1b167350eb38fe6d8880fff5209ee970a9dcccf3
shubhod <shubhodeep.213@gmail.com>
2018-02-25 15:23:48 +0530

    Delete roboserver.js

--- roboserver.js
+++
@@ -1,8 +0,0 @@
-var express= require('express');
-var app=express();
-app.get('/order',(req,res)=>{
-	res.sendFile(__dirname+'/speech.html');
-});
-app.listen(process.env.PORT || 2000,()=>{
-	console.log("roboServer Working properly");
-});
