80dcb535b94cc0a144a11c1aa7ffdecf933ad0b5
shubhod <shubhodeep.213@gmail.com>
2018-03-06 02:11:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,12 @@
 var express = require('express');
+var http = require('http')
+var socketio = require('socket.io');
 var app = express();
-var http = require('http');
 var server = http.Server(app);
 var websocket = socketio(server);
-// set the port of our application
-// process.env.PORT lets the port be set by Heroku
 var port = process.env.PORT || 8080;
-
-// set the view engine to ejs
 app.set('view engine', 'ejs');
-
-// make express look in the public directory for assets (css/js/img)
 app.use(express.static(__dirname + '/public'));
-
-// set the home page route
 app.get('/', function(req, res) {
 
 	// ejs render automatically looks in the views folder
@@ -23,9 +16,4 @@ app.get('/', function(req, res) {
 app.listen(port, function() {
 	console.log('Our app is running on http://localhost:' + port);
 });
-websocket.on('connection', (socket) => {
-  console.log('A client just joined on', socket.id);
-	socket.on("message",(data)=>{
-	console.log(data);
-	});
-});
+
