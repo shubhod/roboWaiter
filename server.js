8b18e1a6b2ba16cd2aad8e79aa1ad19867d052d7
shubhod <shubhodeep.213@gmail.com>
2018-03-06 02:26:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,25 +1,36 @@
-var express = require('express');
-var app = express();
-var http = require('http')
-var socketio = require('socket.io');
-// set the port of our application
-// process.env.PORT lets the port be set by Heroku
-var port = process.env.PORT || 8080;
+// var express = require('express');
+// var app = express();
+// var http = require('http')
+// var socketio = require('socket.io');
+// // set the port of our application
+// // process.env.PORT lets the port be set by Heroku
+// var port = process.env.PORT || 8080;
 
-// set the view engine to ejs
-app.set('view engine', 'ejs');
+// // set the view engine to ejs
+// app.set('view engine', 'ejs');
 
-// make express look in the public directory for assets (css/js/img)
-app.use(express.static(__dirname + '/public'));
+// // make express look in the public directory for assets (css/js/img)
+// app.use(express.static(__dirname + '/public'));
 
-// set the home page route
+// // set the home page route
+// app.get('/', function(req, res) {
+
+//     // ejs render automatically looks in the views folder
+// res.send(__dirname+"/index.html");
+// });
+
+// app.listen(port, function() {
+//     console.log('Our app is running on http://localhost:' + port);
+// });
+var express = require('express'),
+    app = express(),
+    server = require('http').createServer(app),
+    io = require('socket.io').listen(server),
+
+server.listen(process.env.PORT || 3000);
 app.get('/', function(req, res) {
 
     // ejs render automatically looks in the views folder
-res.send(__dirname+"/index.html");
-});
-
-app.listen(port, function() {
-    console.log('Our app is running on http://localhost:' + port);
+res.send("hey");
 });
 
