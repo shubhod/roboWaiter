af9bf1216db1c2bcbd9053ca568da49700d1db1c
shubhod <shubhodeep.213@gmail.com>
2018-03-06 03:13:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -23,8 +23,8 @@
 // });
 var express = require('express'),
     app = express(),
-    server = require('http').createServer(app),
-    io = require('socket.io').listen(server),
+//     server = require('http').createServer(app),
+//     io = require('socket.io').listen(server),
 
 server.listen(process.env.PORT || 3000);
 app.get('/', function(req, res) {
